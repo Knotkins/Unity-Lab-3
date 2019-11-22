7a43e96485d970923a9b3c1be6723ad3ef1d29ff
NanitusJello <44480655+Nanitus@users.noreply.github.com>
2019-11-22 14:06:40 -0500

    A

---
+++ Unity-ECS-RTS/Assets/Components/ShootScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+public GameObject Bullet;
+public Transform Shooter;
+bool isFired = false;
+
+    void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Z) && !isFired)
+            {
+                GameObject bullet = Instantiate(Bullet, Shooter.position, Quaternion.identity);
+                Rigidbody rb = bullet.GetComponent<Rigidbody>();
+                rb.AddForce(Shooter.TransformDirection(Vector3.forward * 500));
+                Destroy(bullet, 2.0f);
+            }
+    }
+}
